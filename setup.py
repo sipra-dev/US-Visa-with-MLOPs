dc440ad53f7a26845472f5768f7c7801c05da194
SIPRA NEYE <sipra.neye@gmail.com>
2026-06-03 14:00:21 +0530

    Fixed package structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="SIPRA NEYE",
+    author_email="sipra.neye@gmail.com",
+    packages=find_packages()
+)
